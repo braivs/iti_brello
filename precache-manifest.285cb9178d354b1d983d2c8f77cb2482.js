28b96c92feb4fa6f12a6faff1f6f73d5ce4d2a38
BriWS <briws@pm.me>
2021-06-12 21:51:54 +0300

    Updates

---
+++ precache-manifest.285cb9178d354b1d983d2c8f77cb2482.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a32735046918751688b0b46548747467",
+    "url": "/iti_brello/index.html"
+  },
+  {
+    "revision": "780bc4808e3e627b06a3",
+    "url": "/iti_brello/static/css/main.76e27cc9.chunk.css"
+  },
+  {
+    "revision": "f7259863db9b06c69735",
+    "url": "/iti_brello/static/js/2.0c958ca7.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/iti_brello/static/js/2.0c958ca7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "780bc4808e3e627b06a3",
+    "url": "/iti_brello/static/js/main.73ed3d86.chunk.js"
+  },
+  {
+    "revision": "ab1282151990dd021c10",
+    "url": "/iti_brello/static/js/runtime-main.1383a8f5.js"
+  }
+]);
