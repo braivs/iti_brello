edc628bd814ebaa72b4b94a0711c192e88456499
Briws <brightwiths@gmail.com>
2022-01-07 18:11:47 +0300

    Restored storybook folders and files. Now it is better. But error in App (storybook): Cannot read properties of undefined (reading 'isLoggedIn')

---
+++ src/features/TodolistsList/Todolist/Task/Task.stories.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import {ComponentMeta, ComponentStory} from '@storybook/react';
+import {Task} from "./Task";
+import {action} from "@storybook/addon-actions";
+import {TaskPriorities, TaskStatuses} from "../../../../api/todolists-api";
+
+const changeTaskStatusCallback = action('Change Task Status')
+const changeTaskTitleCallback = action('Change Task Title')
+const removeTaskCallback = action('Remove Task')
+
+export default {
+  title: 'TODOLIST/Task',
+  component: Task,
+  args: {
+    changeTaskStatus: changeTaskStatusCallback,
+    changeTaskTitle: changeTaskTitleCallback,
+    removeTask: removeTaskCallback
+  },
+} as ComponentMeta<typeof Task>;
+
+const Template: ComponentStory<typeof Task> = (args) => <Task {...args} />;
+
+export const TaskIsDone = Template.bind({});
+TaskIsDone.args = {
+  task: {
+    id: '1', status: TaskStatuses.Completed, title: 'redux', todoListId: 'todo1', description: '',
+    startDate: '', deadline: '', addedDate: '', order: 0, priority: TaskPriorities.Low
+  },
+  todolistId: 'todo1',
+
+};
+export const TaskIsNotDone = Template.bind({});
+TaskIsNotDone.args = {
+  task: {
+    id: '2', status: TaskStatuses.New, title: 'JS', todoListId: 'todo2', description: '',
+    startDate: '', deadline: '', addedDate: '', order: 0, priority: TaskPriorities.Low
+  },
+  todolistId: 'todo2'
+};
+
